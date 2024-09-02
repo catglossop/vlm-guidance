216166da8870ce8a6ef204c3aa5b36008956533d
Catherine Glossop <catherine_glossop@berkeley.edu>
2024-09-01 18:52:21 -0700

    adding debug code

--- debug/compare_conditioning.py
+++ debug/compare_conditioning.py
@@ -34,23 +34,23 @@ IMAGE_SIZE = (96, 96)
 with open(os.path.join(os.path.dirname(__file__), "../data/data_config.yaml"), "r") as f:
     data_config = yaml.safe_load(f)
 
-def transform_images(pil_imgs: List[Image.Image], image_size: List[int], center_crop: bool = False) -> torch.Tensor:
-    """Transforms a list of PIL image to a torch tensor."""
-    if type(pil_imgs) != list:
-        pil_imgs = [pil_imgs]
-    transf_imgs = []
-    for pil_img in pil_imgs:
-        w, h = pil_img.size
-        if center_crop:
-            if w > h:
-                pil_img = TF.center_crop(pil_img, (h, int(h * IMAGE_ASPECT_RATIO)))  # crop to the right ratio
-            else:
-                pil_img = TF.center_crop(pil_img, (int(w / IMAGE_ASPECT_RATIO), w))
-        pil_img = pil_img.resize(image_size) 
-        transf_img = TF.to_tensor(pil_img)
-        transf_img = torch.unsqueeze(transf_img, 0)
-        transf_imgs.append(transf_img)
-    return torch.cat(transf_imgs, dim=1)
+# def transform_images(pil_imgs: List[Image.Image], image_size: List[int], center_crop: bool = False) -> torch.Tensor:
+#     """Transforms a list of PIL image to a torch tensor."""
+#     if type(pil_imgs) != list:
+#         pil_imgs = [pil_imgs]
+#     transf_imgs = []
+#     for pil_img in pil_imgs:
+#         w, h = pil_img.size
+#         if center_crop:
+#             if w > h:
+#                 pil_img = TF.center_crop(pil_img, (h, int(h * IMAGE_ASPECT_RATIO)))  # crop to the right ratio
+#             else:
+#                 pil_img = TF.center_crop(pil_img, (int(w / IMAGE_ASPECT_RATIO), w))
+#         pil_img = pil_img.resize(image_size) 
+#         transf_img = TF.to_tensor(pil_img)
+#         transf_img = torch.unsqueeze(transf_img, 0)
+#         transf_imgs.append(transf_img)
+#     return torch.cat(transf_imgs, dim=1)
 
 def clip_embed(text, device): 
     model_version = "ViT-B/32"
